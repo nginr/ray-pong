610b20f01487bd4f7b58396e377ba2a57bf09974
nginr <zarifhossainafif@gmail.com>
2023-06-17 23:56:23 +0600

    Added point system and reset state after scoring point.

--- main.c
+++ main.c
@@ -17,9 +17,13 @@ typedef struct Paddle {
     int s;
 } Paddle;
 
+void drawMidLine();
+void drawScores();
+
 void makeBall(Ball* b, float cx, float cy, float r, Color c);
 void drawBall(Ball* b);
 void updateBall(Ball* b);
+void resetBall(Ball* b);
 
 void makePaddle(Paddle* p, float x, float y, float w, float h, Color c);
 void drawPaddle(Paddle* p);
@@ -27,9 +31,18 @@ void updatePaddle(Paddle* p);
 
 void updateAI(Paddle* p, Ball* b);
 
-void _bounds_check(float* y, float* h);
 Rectangle _paddle2vec4(Paddle* p);
 Vector2 _ball2vec2(Ball* b);
+
+void _bounds_check(float* y, float* h);
+void _updateScore(Ball* b, Paddle* p, Paddle* ai);
+void _checkBallPaddleCollision(Ball* b, Paddle* p, Paddle* ai);
+
+int _getHalfScreenWidth();
+int _getHalfScreenHeight();
+
+int player_score = 0;
+int ai_score = 0;
 
 int main(void) {
     const int windowWidth = 800;
@@ -54,25 +67,21 @@ int main(void) {
     #define BACKGROUND CLITERAL(Color){ 24, 24, 24, 255 }
 
     while ( !WindowShouldClose() ) {
+        _checkBallPaddleCollision(&ball, &player, &ai);
+        _updateScore(&ball, &player, &ai);
+
+        updateBall(&ball);
+        updatePaddle(&player);
+        updateAI(&ai, &ball);
+
         BeginDrawing();
-            updateBall(&ball);
-            updatePaddle(&player);
-            updateAI(&ai, &ball);
-
-            // Check if the Ball has collided with the paddle
-            if (CheckCollisionCircleRec(_ball2vec2(&ball), ball.radius, _paddle2vec4(&player))) {
-                ball.sx *= -1;
-            }
-            if (CheckCollisionCircleRec(_ball2vec2(&ball), ball.radius, _paddle2vec4(&ai))) {
-                ball.sx *= -1;
-            }
-
             ClearBackground(BACKGROUND);
 
-            DrawLine(windowWidth/2, 0, windowWidth/2, windowWidth, WHITE);
+            drawMidLine();
             drawBall(&ball);
             drawPaddle(&player);
             drawPaddle(&ai);
+            drawScores();
         EndDrawing();
     }
 
@@ -80,6 +89,14 @@ int main(void) {
     return 0;
 }
 
+void drawMidLine() {
+    DrawLine(_getHalfScreenWidth(), 0, _getHalfScreenWidth(), GetScreenHeight(), WHITE);
+}
+
+void drawScores() {
+    DrawText(TextFormat("%i", ai_score), _getHalfScreenWidth()/2 -20, 20, 80, WHITE);
+    DrawText(TextFormat("%i", player_score), 3*_getHalfScreenWidth()/2 -20, 20, 80, WHITE);
+}
 
 void makeBall(Ball* b, float cx, float cy, float r, Color c) {
     b->cx = cx;
@@ -99,12 +116,18 @@ void updateBall(Ball* b) {
     b->cy += b->sy;
 
     // Change direction upon colliding with the walls
-    if (b->cx+b->radius >= GetScreenWidth() || b->cx - b->radius <= 0) {
-        b->sx *= -1;
-    }
-    if (b->cy+b->radius >= GetScreenHeight() || b->cy - b->radius <= 0) {
+    if (b->cy + b->radius >= GetScreenHeight() || b->cy - b->radius <= 0) {
         b->sy *= -1;
     }
+}
+
+void resetBall(Ball* b) {
+    b->cx = GetScreenWidth()*0.5;
+    b->cy = GetScreenHeight()*0.5;
+
+    int speed_choice[2] = {-1, 1};
+    b->sx *= speed_choice[GetRandomValue(0, 1)];
+    b->sy *= speed_choice[GetRandomValue(0, 1)];
 }
 
 void makePaddle(Paddle* p, float x, float y, float w, float h, Color c) {
@@ -128,6 +151,14 @@ void updatePaddle(Paddle* p) {
         p->y += p->s;
     }
     _bounds_check(&p->y, &p->h);
+}
+
+void resetPaddle(Paddle* player, Paddle* ai) {
+    ai->x = 10;
+    ai->y = _getHalfScreenHeight() - ai->h*0.5;
+
+    player->x = GetScreenWidth() - player->w - 10;
+    player->y = _getHalfScreenHeight() - player->h*0.5;
 }
 
 void updateAI(Paddle* p, Ball* b) {
@@ -156,4 +187,35 @@ Rectangle _paddle2vec4(Paddle* p) {
 
 Vector2 _ball2vec2(Ball* b) {
     return (Vector2) {b->cx, b->cy};
+}
+
+void _updateScore(Ball* b, Paddle* p, Paddle* ai){
+    if (b->cx + b->radius >= GetScreenWidth()) {
+        ai_score++;
+        resetBall(b);
+        resetPaddle(p, ai);
+    }
+    if (b->cx - b->radius <= 0) {
+        player_score++;
+        resetBall(b);
+        resetPaddle(p, ai);
+    }
+}
+
+void _checkBallPaddleCollision(Ball* b, Paddle* p, Paddle* ai) {
+    // Check if the Ball has collided with the paddle
+    if (CheckCollisionCircleRec(_ball2vec2(b), b->radius, _paddle2vec4(p))) {
+        b->sx *= -1;
+    }
+    if (CheckCollisionCircleRec(_ball2vec2(b), b->radius, _paddle2vec4(ai))) {
+        b->sx *= -1;
+    }
+}
+
+int _getHalfScreenWidth() {
+    return GetScreenWidth()*0.5;
+}
+
+int _getHalfScreenHeight() {
+    return GetScreenHeight()*0.5;
 }
