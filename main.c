e85e9eef53f5481d46e1dd8827908464902edefd
nginr <zarifhossainafif@gmail.com>
2023-06-17 15:22:35 +0600

    Abstracted the Ball entity in a struct with added functionality.

--- main.c
+++ main.c
@@ -1,5 +1,23 @@
 #include <raylib.h>
 
+
+
+typedef struct Ball {
+    int cx, cy;
+    float radius;
+    Color color;
+
+    int sx, sy;
+} Ball;
+
+void makeBall(Ball* b, int cx, int cy, float r, Color c);
+void drawBall(Ball* b);
+void updateBall(Ball* b);
+void updateBall(Ball* b) {
+    b->cx += b->sx;
+    b->cy += b->sy;
+}
+
 int main(void) {
     const int windowWidth = 800;
     const int windowHeight = 450;
@@ -7,6 +25,9 @@ int main(void) {
 
     const int paddleWidth = 20;
     const int paddleHeight = 120;
+
+    Ball ball = {0};
+    makeBall(&ball, windowWidth/2, windowHeight/2, 10, RED);
 
     InitWindow(windowWidth, windowHeight, windowName);
     SetTargetFPS(60);
@@ -16,8 +37,9 @@ int main(void) {
     while ( !WindowShouldClose() ) {
         BeginDrawing();
             ClearBackground(BACKGROUND);
+
             DrawLine(windowWidth/2, 0, windowWidth/2, windowWidth, WHITE);
-            DrawCircle(windowWidth/2, windowHeight/2, 10, RED);
+            drawBall(&ball);
             DrawRectangle(0 + 10, windowHeight/2 - paddleHeight/2, paddleWidth, paddleHeight, RED);
             DrawRectangle(windowWidth - paddleWidth - 10, windowHeight/2 - paddleHeight/2, paddleWidth, paddleHeight, RED);
         EndDrawing();
@@ -25,4 +47,16 @@ int main(void) {
 
     CloseWindow();
     return 0;
+}
+
+
+void makeBall(Ball* b, int cx, int cy, float r, Color c) {
+    b->cx = cx; b->cy = cy;
+    b->radius = r;
+    b->color = c;
+    b->sx = 0; b->sy = 0;
+}
+
+void drawBall(Ball* b) {
+    DrawCircle(b->cx, b->cy, b->radius, b->color);
 }
