88af0879d213c44f1c4fd4bc3e847307beca8119
nginr <zarifhossainafif@gmail.com>
2023-06-17 14:41:09 +0600

    Added all the visual entites (Paddles, Ball and the equator).

--- main.c
+++ main.c
@@ -5,12 +5,21 @@ int main(void) {
     const int windowHeight = 450;
     const char* windowName = "Pong";
 
+    const int paddleWidth = 20;
+    const int paddleHeight = 120;
+
     InitWindow(windowWidth, windowHeight, windowName);
     SetTargetFPS(60);
 
+    #define BACKGROUND CLITERAL(Color){ 24, 24, 24, 255 }
+
     while ( !WindowShouldClose() ) {
         BeginDrawing();
-            ClearBackground(BLACK);
+            ClearBackground(BACKGROUND);
+            DrawLine(windowWidth/2, 0, windowWidth/2, windowWidth, WHITE);
+            DrawCircle(windowWidth/2, windowHeight/2, 10, RED);
+            DrawRectangle(0 + 10, windowHeight/2 - paddleHeight/2, paddleWidth, paddleHeight, RED);
+            DrawRectangle(windowWidth - paddleWidth - 10, windowHeight/2 - paddleHeight/2, paddleWidth, paddleHeight, RED);
         EndDrawing();
     }
 
